9c1f869d1aeb601f0ef6cdf213f7abd6c4682311
OFUTUROHEAGORA <ofuturoheagora@gmail.com>
2023-01-30 15:04:22 -0300

    Criando a app Empresa

---
+++ empresa/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Empresa
+
+# Register your models here.
+
+admin.site.register(Empresa)
---
+++ empresa/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Empresa(models.Model):
+    nome = models.CharField(max_length=100, help_text='Nome da empresa')
+    
