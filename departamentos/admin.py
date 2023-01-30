e3c5e25cfcb7458b2b7051a04a0cf37085fdefba
OFUTUROHEAGORA <ofuturoheagora@gmail.com>
2023-01-30 15:30:04 -0300

    Criando a app Departamentos

---
+++ departamentos/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Departamento
+
+admin.site.register(Departamento)
+
+# Register your models here.
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'empresa',
-    'Funcionarios'
+    'Funcionarios',
+    'departamentos',
 ]
 
 MIDDLEWARE = [
