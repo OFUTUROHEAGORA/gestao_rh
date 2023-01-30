b166772983f5e82b572af45a12888cc5e5cbeec4
OFUTUROHEAGORA <ofuturoheagora@gmail.com>
2023-01-30 15:18:49 -0300

    Criando a app Funcionarios

---
+++ Funcionarios/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Funcionario
+
+# Register your models here.
+
+admin.site.register(Funcionario)
---
+++ Funcionarios/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
