a9781fe154ad12df55d50293d464e39f32921ded
OFUTUROHEAGORA <ofuturoheagora@gmail.com>
2023-02-12 22:28:26 -0300

    Criando os relacionamento entre os dados

--- Funcionarios/models.py
+++ Funcionarios/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from departamentos.models import Departamento
+from empresa.models import Empresa
 
 # Create your models here.
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamento = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
     
     def __str__(self):
         return self.nome
